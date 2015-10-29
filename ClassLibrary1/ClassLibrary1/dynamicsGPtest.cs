170b80e5e0885f6d032d35943309c79f90abf160
alko0314 <konovalcev@me.com>
2015-10-29 13:20:47 +0300

    test

---
+++ ClassLibrary1/ClassLibrary1/dynamicsGPtest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.Dexterity.Bridge;
+using Microsoft.Dexterity.Applications;
+using Microsoft.Dexterity.Applications.DynamicsDictionary;
+//using Microsoft.Office.Interop.Excel;
+
+namespace GP2015
+{
+
+    public class GPAddIn : IDexterityAddIn
+    {
+        // Add a local variable to the PM Vendor Maintenance
+        private PmVendorMaintenanceForm pmTrxForm = Dynamics.Forms.PmVendorMaintenance;
+
+        public void Initialize()
+        {
+            // Create a subscription to the event of the PM Vendor window opening
+            this.pmTrxForm.PmVendorMaintenance.OpenAfterOriginal += new EventHandler(OnPMVendornOpened);
+        }
+
+        // Create a method that is called when the PM Vendor window is opened
+        private void OnPMVendornOpened(object sender, EventArgs e)
+        {
+            // Add a default value to the open PmVendorMaintenance fields
+            this.pmTrxForm.PmVendorMaintenance.City.Value = "Barcelona";
+            this.pmTrxForm.PmVendorMaintenance.Country.Value = "Spain";
+        }
+    }
+}
