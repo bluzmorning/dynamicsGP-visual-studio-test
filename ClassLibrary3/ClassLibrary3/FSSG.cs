ed484bf682835e48b2c7a13977b0a25480106a8d
alko0314 <konovalcev@me.com>
2015-10-31 02:36:27 +0300

    test2

--- ClassLibrary3/ClassLibrary3/FSSG.cs
+++ ClassLibrary3/ClassLibrary3/FSSG.cs
@@ -27,16 +27,29 @@ namespace GP2015
             this.pmTrxForm.PmVendorMaintenance.Country.Value = "Spain";
         }
         // Create a method that is called when the testExcel button is pushed
-        static void CreateExcel(object sender, EventArgs e)
+        private void CreateExcel(object sender, EventArgs e)
         {
             Excel.Application excel = new Excel.Application();
-            excel.Visible = true;
-            Excel.Workbook wb = excel.Workbooks.Open("testdata.xlsx");
+            excel.Visible = false;
+            excel.DisplayAlerts = false;
+            Excel.Workbook wb = excel.Workbooks.Add();
             Excel.Worksheet sh = wb.Sheets.Add();
-            sh.Name = "TestSheet";
-            sh.Cells[1, "A"].Value2 = "SNO";
-            sh.Cells[2, "B"].Value2 = "A";
-            sh.Cells[2, "C"].Value2 = "1122";
+            sh.Name = "JPMorgan";
+            sh.Cells[1, "A"].Value = "Vendor Name";
+            sh.Cells[1, "B"].Value = "City";
+            sh.Cells[1, "C"].Value = "Zip Code";
+            sh.Cells[1, "D"].Value = "Tax ID";
+            sh.Cells[1, "E"].Value = "Phone";
+            sh.Cells[1, "F"].Value = "Email";
+            sh.Cells[1, "G"].Value = "Currency ID";
+            sh.Cells[2, "A"].Value = this.pmTrxForm.PmVendorMaintenance.VendorName.Value;
+            sh.Cells[2, "B"].Value = this.pmTrxForm.PmVendorMaintenance.City.Value;
+            sh.Cells[2, "C"].Value = this.pmTrxForm.PmVendorMaintenance.ZipCode.Value;
+            sh.Cells[2, "D"].Value = this.pmTrxForm.PmVendorMaintenanceAdditionalInformation.TaxIdNumber.Value;
+            sh.Cells[2, "E"].Value = this.pmTrxForm.PmVendorMaintenance.PhoneNumber1.Value;
+            sh.Cells[2, "F"].Value = this.pmTrxForm.PmVendorMaintenance.Comment1.Value;
+            sh.Cells[2, "G"].Value = this.pmTrxForm.PmVendorMaintenanceAdditionalInformation.CurrencyId.Value;
+            wb.SaveAs(@"D:\Excel\testdata.xlsx");
             wb.Close(true);
             excel.Quit();
         }
